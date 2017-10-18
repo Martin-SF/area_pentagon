5ef57c2424e7a69091c2d0c48f6e24f7299de845
Martin Schönfeld <32391476+Martin-SF@users.noreply.github.com>
2017-10-18 10:27:10 +0200

    Update area.py
    minor changes for better overview

--- area.py
+++ area.py
@@ -1,15 +1,14 @@
 import numpy as np
 
-e = 5
+e = 5 #number of corners
+b0 = np.sqrt(4*1/np.sqrt(25+10*np.sqrt(5))) 
+
+# area=1 for this values: 
 #b0 = np.sqrt(2)
 #b0 = np.sqrt(2/(np.sqrt(27))) 6eck
-b0 = np.sqrt(4*1/np.sqrt(25+10*np.sqrt(5))) 
-# area=1 for this value 
 
-#pi = 3.14159265359
-pi = np.pi
-phi = pi*(1-(2/e))
-alpha = (pi-phi)/2
+phi = np.pi*(1-(2/e))
+alpha = (np.pi-phi)/2
 gamma = np.sqrt(2*(1-np.cos(phi)))
 
 Pc = gamma-2*1/gamma
@@ -23,10 +22,10 @@ def part_area_pentagon(j):
 	return e*1/2*(np.sin(alpha)*b*d+(c-2*d)*np.sin(phi)*d)
 
 values = []
-lol = 0
+area = 0
 for i in range(0, 100):
   values.append(part_area_pentagon(i))
-  lol += values[i]
+  area += values[i]
   
 
 np.sum(values)  #no output!?!
